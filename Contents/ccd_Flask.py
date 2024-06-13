5472e25c3c5d32e6661ca15f7dd1acfcd790b1d0
PATEL DARSH <147577241+darshpatel1052@users.noreply.github.com>
2024-06-13 15:08:56 +0530

    Add files via upload

---
+++ Contents/ccd_Flask.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, jsonify
+import numpy as np
+import joblib
+
+app = Flask(__name__)
+
+rf = joblib.load('D:/ML/100_days_of_ml/model.joblib')
+pipeline=joblib.load('D:/ML/100_days_of_ml/pipeline.joblib')
+
+@app.route('/')
+def home():
+    return render_template('ccd_.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json()
+    query_point = np.array(data['query_point']).reshape(1, -1)
+    query_point_transformed = pipeline.transform(query_point)
+    prediction = rf.predict(query_point_transformed)
+    return jsonify({'prediction': prediction.tolist()})
+
+if __name__ == '__main__':
+    app.run(debug=True)
